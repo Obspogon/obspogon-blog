28275544740b7f01d15042c7f0ffc61ebc6232ce
Obspogon <72287594+Obspogon@users.noreply.github.com>
2022-05-04 21:30:44 -0400

    improvments

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,15 @@
+const blogTools = require("eleventy-plugin-blog-tools"); // https://plug11ty.com/plugins/blog-tools/
+const readerBar = require("eleventy-plugin-reader-bar"); // https://plug11ty.com/plugins/reader-bar/
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src");
-
-	// eleventyConfig.addFilter("postContent", function (post) {
-	// 	return post.template.inputContent;
-	// });
 
 	eleventyConfig.addFilter("reverse", (arr) => {
 		arr.reverse();
 	});
 
-	// eleventyConfig.addWatchTarget("./style.css");
-	eleventyConfig.addShortcode("excerpt", (article) => extractExcerpt(article));
+	eleventyConfig.addPlugin(blogTools);
+	eleventyConfig.addPlugin(readerBar);
 
 	eleventyConfig.setBrowserSyncConfig({
 		open: true,
@@ -27,31 +26,3 @@ module.exports = function (eleventyConfig) {
 		},
 	};
 };
-
-function extractExcerpt(article) {
-	if (!article.hasOwnProperty("templateContent")) {
-		console.warn('Failed to extract excerpt: Document has no property "templateContent".');
-		return null;
-	}
-
-	let excerpt = null;
-	const content = article.templateContent;
-
-	// The start and end separators to try and match to extract the excerpt
-	const separatorsList = [
-		{ start: "<!-- Excerpt Start -->", end: "<!-- Excerpt End -->" },
-		{ start: "<p>", end: "</p>" },
-	];
-
-	separatorsList.some((separators) => {
-		const startPosition = content.indexOf(separators.start);
-		const endPosition = content.indexOf(separators.end);
-
-		if (startPosition !== -1 && endPosition !== -1) {
-			excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
-			return true; // Exit out of array loop on first match
-		}
-	});
-
-	return excerpt;
-}
--- src/credits.md
+++ src/credits.md
@@ -10,6 +10,6 @@ tags: ["section"]
 -   Backgrounds from [WallpaperCave](https://wallpapercave.com/) and [Subtle Patterns](https://www.toptal.com/designers/subtlepatterns/).
 -   Blog layout based on [eev.ee/fuzzy notepad](https://eev.ee/blog/)
 -   Fonts
-    -   Main: Open Sans and Roboto Mono from Google Fonts
-    -   Title: [DaFont](https://www.dafont.com/)
+    -   Main: [Open Sans](https://fonts.google.com/specimen/Open+Sans) and [Roboto Mono](https://fonts.google.com/specimen/Roboto+Mono) from [Google Fonts](https://fonts.google.com/)
+    -   Title: [Abstract](https://www.dafont.com/abstract.font) from [DaFont](https://www.dafont.com/)
 -   Preloading with [instant.page](https://instant.page/)
